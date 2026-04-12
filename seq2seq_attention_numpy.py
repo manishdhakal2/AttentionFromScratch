dd31f0d65ae508b9afe8ad2c66e2ad616690c1d2
Manish Dhakal <dhakalmanish398@gmail.com>
2026-04-12 18:47:21 +0545

    Feat : Separate the computation of context vector into a function for modularization and reusing

--- seq2seq_attention_numpy.py
+++ seq2seq_attention_numpy.py
@@ -4,6 +4,8 @@ import numpy as np
 class Attention:
     def __init__(self):
 
+        self.general_w = None
+
 
         pass
 
@@ -26,6 +28,41 @@ class Attention:
             similarity_score[index] = np.dot(h_enc[index], h_dec_t[index])
 
 
+        return self.get_context_vector(h_enc, similarity_score)
+
+
+
+
+    def general_attention(self,h_enc, h_dec_t):
+        """
+        Returns the general weighted  attention of the given encoder and decoder hidden states
+
+        Params :
+            h_enc (B,T,H): Encoder hidden state across all timesteps
+            h_dec_t(B,H) :  Current Decoder hidden state  
+
+        """
+
+        B,T,H = h_enc.shape
+
+
+        self.general_w = np.zeros((B,T,H))
+
+        
+
+
+
+    def softmax(self,x):
+
+        """
+        Returns the softmax of the given input vector
+        """
+        return np.exp(x)/np.sum(np.exp(x), axis =1, keepdims = True)
+    
+    
+    def get_context_vector(self, h_enc, similarity_score):
+        B,T,H = h_enc.shape
+
         softmaxed_score = self.softmax(similarity_score)
 
         context_vector = np.zeros((B,H))
@@ -38,11 +75,6 @@ class Attention:
 
 
 
-    def softmax(self,x):
 
-        """
-        Returns the softmax of the given input vector
-        """
-        return np.exp(x)/np.sum(np.exp(x), axis =1, keepdims = True)
 
 
