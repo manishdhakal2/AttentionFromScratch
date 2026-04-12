6840b53f36b54482d86c15a52f94a311e1a925fb
Manish Dhakal <dhakalmanish398@gmail.com>
2026-04-12 22:56:55 +0545

    Add logic for concat attention and fix minor bugs in general attention

--- seq2seq_attention_numpy.py
+++ seq2seq_attention_numpy.py
@@ -5,8 +5,8 @@ class Attention:
     def __init__(self):
 
         self.general_w = None
-
-
+        self.concat_w = None
+        self.concat_v = None
         pass
 
     def dot_attention(self,h_enc,h_dec_t):
@@ -30,9 +30,6 @@ class Attention:
 
         return self.get_context_vector(h_enc, similarity_score)
 
-
-
-
     def general_attention(self,h_enc, h_dec_t):
         """
         Returns the general weighted  attention of the given encoder and decoder hidden states
@@ -42,25 +39,53 @@ class Attention:
             h_dec_t(B,H) :  Current Decoder hidden state  
 
         """
-
+        
         B,T,H = h_enc.shape
 
         #Initialize the weight matrix randomly
-        if not self.general_w:
+        if self.general_w is None:
             self.general_w = np.random.uniform(0,1,(H,H)) 
 
-        
-
-        transformed_h_dec = np.zeros((B,H))
-
-        for index in range(B):
-            transformed_h_dec[index] = h_dec_t[index] @ self.general_w
+        transformed_h_dec = h_dec_t @ self.general_w
         
         similarity_score = np.zeros((B,T))
 
         for index in range(B):
             similarity_score[index] = np.dot(h_enc[index],  transformed_h_dec[index])
         
+        return self.get_context_vector(h_enc, similarity_score)
+    
+
+
+    def concat_attention(self, h_enc, h_dec_t):
+        """
+        Returns the general weighted  attention of the given encoder and decoder hidden states
+
+        Params :
+            h_enc (B,T,H): Encoder hidden state across all timesteps
+            h_dec_t(B,H) :  Current Decoder hidden state  
+
+        """
+        D = 64
+        B,T,H = h_enc.shape
+
+        if self.concat_w is None:
+            self.concat_w = np.random.uniform(0,1,(2*H,D))
+        
+        #Make (B,H) -> (B,1,H) and repeat T times -> (B,T,H)
+        repeated_h_dec = np.repeat(h_dec_t[:,None,:], T, axis =1)
+
+        #Concat Repeated h_dec and h_enc
+        joint_states = np.concatenate([repeated_h_dec, h_enc],axis = 2)
+
+        #Multiply W and [Q;K]
+        transformed_states = joint_states @ self.concat_w
+
+        if self.concat_v is  None:
+            self.concat_v = np.random.uniform(0,1,(D,))
+
+        #Get similarity score
+        similarity_score = transformed_states @ self.concat_v
 
         return self.get_context_vector(h_enc, similarity_score)
 
@@ -72,8 +97,8 @@ class Attention:
         Returns the softmax of the given input vector
         """
         return np.exp(x)/np.sum(np.exp(x), axis =1, keepdims = True)
-    
-    
+
+
     def get_context_vector(self, h_enc, similarity_score):
         B,T,H = h_enc.shape
 
