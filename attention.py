Fix : Correct the matrix shape of key in dot computation and fix shape initialization of w_V

attention.py
@@ -6,7 +6,7 @@ class SelfAttention(nn.Module):
         super().__init__()
 
         #Assumes dimension of key is equal to dimension of value
-        self.d_k = d_k
+        self.d_k = None
 
         #Dimension for the weights
         self.D_w = 64
@@ -30,14 +30,16 @@ class SelfAttention(nn.Module):
         if self.w_K is None:
             self.w_K = torch.rand([E, self.D_w], requires_grad= True)
         if self.w_V is None:
-            self.w_V = torch.rand([E, self.D_w], requires_grad= True)
+            self.w_V = torch.rand([E, V], requires_grad= True)
 
         query = embeddings @ self.w_Q
         key = embeddings @ self.w_K
         value = embeddings @ self.w_V
+        self.d_k = torch.tensor(key.shape[2])
 
         #Compute dot product
-        dot_qk = query @ key.T
+        dot_qk = query @ key.permute((0,2,1))
+
 
         #Normalize the dot product
         normalized_dot_qk = dot_qk/torch.sqrt(self.d_k)
@@ -49,6 +51,11 @@ class SelfAttention(nn.Module):
         attention_qkv = softmaxed_dot_qk @ self.w_V.T
 
         return attention_qkv
+
+
+
+
+attention = SelfAttention(3)
 
 
 